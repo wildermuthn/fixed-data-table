620253b8e8fd608258e188864add4fdaa5491874
Nate Wildermuth <nwildermuth@nowthismedia.com>
2015-02-20 12:48:24 -0600

    Added object.assign polyfill

--- dist/fixed-data-table.js
+++ dist/fixed-data-table.js
@@ -9,6 +9,39 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
+// Object.assign Polyfill
+if (!Object.assign) {
+  Object.defineProperty(Object, 'assign', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function(target, firstSource) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert first argument to object');
+      }
+
+      var to = Object(target);
+      for (var i = 1; i < arguments.length; i++) {
+        var nextSource = arguments[i];
+        if (nextSource === undefined || nextSource === null) {
+          continue;
+        }
+
+        var keysArray = Object.keys(Object(nextSource));
+        for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+          var nextKey = keysArray[nextIndex];
+          var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+          if (desc !== undefined && desc.enumerable) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+      return to;
+    }
+  });
+}
+
 var FixedDataTable =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
